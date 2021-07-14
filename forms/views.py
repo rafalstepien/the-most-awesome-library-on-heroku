0f03afd4777d8487afb31995bad793d3f1566dba
Rafal Stepien <rafal.stepien@mnm.bio>
2021-07-14 18:51:21 +0200

    Add saving the input book to the database if input is correct

--- forms/views.py
+++ forms/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponseRedirect
 from forms import forms
+from the_most_awesome_library.models import Book
 
 
 def submit_new_book(request):
@@ -8,8 +9,27 @@ def submit_new_book(request):
         form = forms.AddBookForm(request.POST)
 
         if form.is_valid():
-            return HttpResponseRedirect('/library')
+            title = form.cleaned_data.get('title')
+            author = form.cleaned_data.get('author')
+            publication_date = form.cleaned_data.get('publication_date')
+            isbn_number = form.cleaned_data.get('isbn_number')
+            pages_number = form.cleaned_data.get('pages_number')
+            cover_link = form.cleaned_data.get('cover_link')
+            language = form.cleaned_data.get('language')
 
+            book = Book(
+                title=title,
+                author=author,
+                publication_date=publication_date,
+                isbn_number=isbn_number,
+                pages_number=pages_number,
+                cover_link=cover_link,
+                language=language,
+            )
+
+            book.save()
+
+            return HttpResponseRedirect('/library')
         else:
             print(form.errors)
 
