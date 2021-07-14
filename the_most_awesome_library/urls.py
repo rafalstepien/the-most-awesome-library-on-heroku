1d3bfef8e5bf0a2ba9cd93d3b374047c95b439ca
Rafal Stepien <rafal.stepien@mnm.bio>
2021-07-14 18:35:39 +0200

    Add upgraded view of edit page

--- the_most_awesome_library/urls.py
+++ the_most_awesome_library/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-
 from . import views
+from forms import views as edit_page_views
+
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('all_books/', views.all_books_view, name='all_books'),
-    path('edit/', views.edit_view, name='edit'),
+    path('edit/', edit_page_views.submit_new_book, name='edit'),
 ]
