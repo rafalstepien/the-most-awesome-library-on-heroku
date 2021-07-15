d6796a485b272a2c51ef50f51557ec3fa1e31e21
Rafal Stepien <rafal.stepien@mnm.bio>
2021-07-15 12:26:46 +0200

    Add unittests for resolving URLs

--- the_most_awesome_library/tests.py
+++ the_most_awesome_library/tests.py
@@ -1,10 +1,14 @@
 from django.test import TestCase
+from django.urls import resolve
+import the_most_awesome_library 
+import forms
 
 
 class HomePageTest(TestCase):
     
     def test_root_resolves_to_home_page_view(self):
-        pass
+        resolved_function = resolve('/')
+        self.assertEqual(resolved_function.func, the_most_awesome_library.views.index)
 
     def test_home_page_view_contains_correct_html(self):
         pass
@@ -13,7 +17,8 @@ class HomePageTest(TestCase):
 class BooksPageTest(TestCase):
 
     def test_books_resolves_to_all_books_view(self):
-        pass
+        resolved_function = resolve('/all_books/')
+        self.assertEqual(resolved_function.func, the_most_awesome_library.views.all_books_view)
 
     def test_books_view_contains_correct_html(self):
         pass
@@ -22,7 +27,8 @@ class BooksPageTest(TestCase):
 class EditPageTest(TestCase):
 
     def test_edit_resolves_to_edit_books_view(self):
-        pass
+        resolved_function = resolve('/edit/')
+        self.assertEqual(resolved_function.func, forms.views.submit_new_book)
 
     def test_edit_view_contains_correct_html(self):
         pass
